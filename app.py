34564b253fde6b36bc94886990ccba06df7df749
sivaramkumar.mani@charter.com <sivaramkumar.mani@charter.com>
2023-09-30 08:52:19 -0600

    HTTP Verbs

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import json
+import sqlite3
 
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
 
 app = Flask(__name__)
+
+db_connect = sqlite3.connect('database.db')
+db_connect.execute(
+    'CREATE TABLE IF NOT EXISTS Users (name TEXT, \
+    email TEXT, city TEXT, country TEXT, phone TEXT, age INTEGER)')
 
 
 # default landing
@@ -20,4 +26,27 @@ def about():
     return response
 
 
-app.run(port=8000, debug=True)
+# default landing
+# @app.route('/api/user')
+# def user():
+#     res = {"name": "siva", "age": "40"}
+#
+#     response = jsonify(res)
+#     return response
+
+
+# default landing
+@app.route('/api/city')
+def city():
+    res = {"name": "pondicherry", "country": "india"}
+    jres = json.dumps(res)
+    response = Response(jres, mimetype="application/json", status=200)
+    return response
+
+
+@app.route('/api/user', methods=['POST', 'GET', 'DELETE'])
+def add_user():
+    return jsonify({"name": "siva", "age": "40"})
+
+
+app.run(port=5000, debug=True)
